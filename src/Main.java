3927546b1313fbbe3a15be3b13a7b59f12b2b59b
Matheus Waked Franca <matheus363@hotmail.com>
2023-03-23 23:31:06 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 package src;
 
+import src.com.waked.challenge.domain.Bootcamp;
 import src.com.waked.challenge.domain.Course;
+import src.com.waked.challenge.domain.Dev;
 import src.com.waked.challenge.domain.Mentorship;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 public class Main {
 
@@ -23,8 +26,32 @@ public class Main {
         mentorship.setDescription("java mentorship description");
         mentorship.setDate(LocalDate.now());
 
-        System.out.println(course1);
-        System.out.println(course2);
-        System.out.println(mentorship);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Java Developer Bootcamp");
+        bootcamp.setDescription("Java Developer Bootcamp Description");
+        bootcamp.getContents().addAll(Set.of(course1, course2, mentorship));
+
+        Dev devMatheus = new Dev();
+        devMatheus.setName("Matheus");
+        devMatheus.subscribeBootcamp(bootcamp);
+        System.out.println("Subscribed content = " + devMatheus.getContentsSubscribed());
+        System.out.println("---");
+        devMatheus.progress();
+        System.out.println("Finished content = " + devMatheus.getContentsFinished());
+        System.out.println("Subscribed content = " + devMatheus.getContentsSubscribed());
+        System.out.println("XP: " + devMatheus.calculateTotalXP());
+
+        System.out.println("----------------");
+        Dev devTalita = new Dev();
+        devTalita.setName("Talita");
+        devTalita.subscribeBootcamp(bootcamp);
+        System.out.println("Subscribed content = " + devTalita.getContentsSubscribed());
+        devTalita.progress();
+        devTalita.progress();
+        devTalita.progress();
+        System.out.println("---");
+        System.out.println("Finished content = " + devTalita.getContentsFinished());
+        System.out.println("Subscribed content = " + devTalita.getContentsSubscribed());
+        System.out.println("XP: " + devTalita.calculateTotalXP());
     }
 }
